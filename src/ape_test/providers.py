3e29a6732ee3418aef6051c53f7f2377d3562900
unparalleled-js <yingthi@live.com>
2021-11-11 12:20:57 -0600

    feat: subclass web3 provider

--- src/ape_test/providers.py
+++ src/ape_test/providers.py
@@ -1,17 +1,13 @@
-from typing import Iterator
-
 from eth_tester.backends import PyEVMBackend  # type: ignore
 from eth_tester.exceptions import TransactionFailed  # type: ignore
 from eth_utils.exceptions import ValidationError
 from web3 import EthereumTesterProvider, Web3
 
-from ape.api import ReceiptAPI, TestProviderAPI, TransactionAPI
+from ape.api import ReceiptAPI, TestProviderAPI, TransactionAPI, Web3Provider
 from ape.exceptions import ContractLogicError, OutOfGasError, VirtualMachineError
 
 
-class LocalNetwork(TestProviderAPI):
-    _web3: Web3 = None  # type: ignore
-
+class LocalNetwork(TestProviderAPI, Web3Provider):
     def connect(self):
         pass
 
@@ -32,35 +28,11 @@ class LocalNetwork(TestProviderAPI):
             err_message = str(err).split("execution reverted: ")[-1]
             raise ContractLogicError(err_message) from err
 
-    @property
-    def chain_id(self) -> int:
-        return self._web3.eth.chain_id
-
-    @property
-    def gas_price(self) -> int:
-        # NOTE: Test chain doesn't care about gas prices
-        return 0
-
-    def get_nonce(self, address: str) -> int:
-        return self._web3.eth.get_transaction_count(address)  # type: ignore
-
-    def get_balance(self, address: str) -> int:
-        return self._web3.eth.get_balance(address)  # type: ignore
-
-    def get_code(self, address: str) -> bytes:
-        return self._web3.eth.get_code(address)  # type: ignore
-
     def send_call(self, txn: TransactionAPI) -> bytes:
         data = txn.as_dict()
         if "gas" not in data or data["gas"] == 0:
             data["gas"] = int(1e12)
         return self._web3.eth.call(data)
-
-    def get_transaction(self, txn_hash: str) -> ReceiptAPI:
-        # TODO: Work on API that let's you work with ReceiptAPI and re-send transactions
-        receipt = self._web3.eth.wait_for_transaction_receipt(txn_hash)  # type: ignore
-        txn = self._web3.eth.get_transaction(txn_hash)  # type: ignore
-        return self.network.ecosystem.receipt_class.decode({**txn, **receipt})
 
     def send_transaction(self, txn: TransactionAPI) -> ReceiptAPI:
         try:
@@ -76,9 +48,6 @@ class LocalNetwork(TestProviderAPI):
             raise OutOfGasError()
 
         return receipt
-
-    def get_events(self, **filter_params) -> Iterator[dict]:
-        return iter(self._web3.eth.get_logs(filter_params))  # type: ignore
 
     def snapshot(self) -> str:
         return self._tester.take_snapshot()
